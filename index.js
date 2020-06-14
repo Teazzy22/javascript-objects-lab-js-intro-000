187b9d192ae28047b07c3fcf72972b20ea4bffd2
Kyle Nugent <kln5656@gmail.com>
2020-06-14 02:08:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 var recipes = {}
-var recipes = updateObjectWithKeyAndVale({object:'ball', key:'skeloton', value:'22'});
+var updateObjectWithKeyAndVale = new recipes({object:'ball', key:'skeloton', value:'22'});
